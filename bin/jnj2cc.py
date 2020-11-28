9f0af29e66c63f0d407b4421523d2e42af1f9996
peitur <peitur@gmail.com>
2020-11-28 02:07:20 +0100

    skel

---
+++ bin/jnj2cc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import sys, os, re
+import json, yaml, jinja2
+from pprint import pprint
+
+
+if __name__ == "__main__":
+
+    pass
---
+++ bin/yaml2env.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import sys, os, re
+import yaml
+from pprint import pprint
+
+
+if __name__ == "__main__":
+
+    pass
