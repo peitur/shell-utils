c4a10e2260c3718350e35e26ae614c8689b535cb
peitur <peitur@gmail.com>
2020-11-28 03:40:04 +0100

    skel and test

--- bin/jnj2cc.py
+++ bin/jnj2cc.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 
-
 import sys, os, re
-import json, yaml, jinja2
+import json, jinja2
+import pathlib
 from pprint import pprint
 
+def read_json( filename, **opt ):
+    return json.load( open( filename, "r" ))
+
+def read_textfile( filename, **opt ):
+    ret = list()
+    with open( filename, "r") as f:
+        for line in f.readlines():
+            ret.append( line )
+    return "".join( ret )
+
+
+def print_usage( tool ):
+    print("%s <varfile>.json <templatefile>" % ( tool ))
 
 if __name__ == "__main__":
 
-    pass
+    opt = dict()
+
+    if len( sys.argv ) < 3:
+        print_usage( sys.argv[0] )
+        raise AttributeError( "Need input files to work woth, variables file and a template file!" )
+
+    opt['varfile'] = sys.argv[1]
+    opt['templatefile'] = sys.argv[2]
+
+    try:
+        varpath = pathlib.Path( opt['varfile'] )
+        templpath = pathlib.Path( opt['varfile'] )
+
+        if not varpath.exists():
+            raise IOError( "No such variables file %s" % ( varpath ) )
+
+        if not templpath.exists():
+            raise IOError( "No such template file %s" % ( templpath ) )
+
+        variables = read_json( str( varpath ) )
+
+        template = jinja2.Template( read_textfile( templpath, **opt ) )
+        print( template.render( variables ))
+
+
+    except IOError as e:
+        print( "ERROR: %s" % (e) )
+    except json.decoder.JSONDecodeError as e:
+        print( "ERROR: Failed to parse JSON file, malformed or not JSON : %s" % (e) )
+    except Exception as e:
+        raise e
--- bin/json2env.py
+++ bin/json2env.py
@@ -2,10 +2,64 @@
 
 
 import sys, os, re
+import pathlib
 import json
 from pprint import pprint
 
+def read_json( filename, **opt ):
+    return json.load( open( filename, "r" ))
+
+def read_textfile( filename, **opt ):
+    ret = list()
+    with open( filename, "r") as f:
+        for line in f.readlines():
+            ret.append( line )
+    return "".join( ret )
+
+def print_source( slines, **opt ):
+    for line in slines:
+        print( line )
+
+def print_usage( tool ):
+    print("%s <inputfile>.json" % ( tool ))
+
+## Basically, get all leaves while tracking the path to them (through lists and dicts)
+def convert( data, key=None ):
+    ret = list()
+    
+
+
+    return ret
+
+
+def istype( obj, t ):
+    if type( obj ).__name__ == t:
+        return True
+    return False
+
+def gettype( obj ):
+    return type( obj ).__name__
 
 if __name__ == "__main__":
+    opt = dict()
 
-    pass
+    if len( sys.argv ) < 2:
+        print_usage( sys.argv[0] )
+        raise AttributeError( "Need input file as only argument" )
+
+    opt['inputfile'] = sys.argv[1]
+
+    try:
+        fpath = pathlib.Path( opt['inputfile'] )
+        if not fpath.exists():
+            raise IOError( "No such file %s" % ( fpath ) )
+
+        ## This tool always asumes json in file
+        print_source( convert( read_json( str( fpath ) ) ) )
+
+    except IOError as e:
+        print( "ERROR: %s" % (e) )
+    except json.decoder.JSONDecodeError as e:
+        print( "ERROR: Failed to parse JSON file, malformed or not JSON : %s" % (e) )
+    except Exception as e:
+        raise e
