debe618222b3cd84e66ad58a56c5909cf2c02415
peitur <peitur@gmail.com>
2021-04-13 12:25:16 +0200

    added support for subdirs by args list

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-CPWD=${PWD}
-for X in $( ls -1 ${CPWD} ); do
-	if [[ -d ${CPWD}/${X} ]]; then
-		echo "INFO: Checking ${X}"
-		if [[ -d ${CPWD}/${X}/.git ]]; then
-			cd ${CPWD}/$X
-			git pull
-			cd -
+XPWD=${PWD}
+ARGS="$@"
+
+if [[ $# == 0 ]]; then
+	ARGS="."
+fi
+
+for V in ${ARGS[@]}; do
+
+	CPWD="${XPWD}/$V"
+	echo "[ ] INFO: Checking everything in ${CPWD}"
+
+	for X in $( ls -1 ${CPWD} ); do
+		if [[ -d ${CPWD}/${X} ]]; then
+			echo "[+] INFO: Checking ${CPWD}/${X}"
+			if [[ -d ${CPWD}/${X}/.git ]]; then
+				cd ${CPWD}/$X
+				git pull
+				cd ${CPWD}
+			fi
 		fi
-	fi
+	done
+
 done
